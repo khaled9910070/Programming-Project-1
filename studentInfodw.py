a3e7bb8b1d925fac003dc2384e5acae3a95c2bf2
rmit-s3553348-louis-abasta <s3553348@student.rmit.edu.au>
2019-08-12 17:44:12 +1000

    Add files via upload
    Updated studentinfodw.py
    - merged studentinfo and studentAssessment data set
    -shows the breakdown of min/max/mean of student performance on assessment
    -shows the breakdown of each assessment grouped by student id and region 
    -created new csv files that show the records

--- studentInfodw.py
+++ studentInfodw.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from statistics import mean
 
 def cleand4():
     d4 = pd.read_csv("studentInfo.csv")
@@ -21,6 +22,10 @@ def disabilityDataSet():
     # does having a disability a factor to the overall performance of a student in regards to their final result
     d4 = pd.read_csv("studentInfo.csv")
     d4 = d4.set_index(['code_module', 'region'])
+
+    mapping = {'Withdrawn': 0, 'Fail': 1, 'Pass': 2, 'Distinction': 3}
+    d4 = d4.replace({'final_result': mapping})
+    #mapping to change the final results into numeric value
     df = d4[(d4['disability']== 'Y')]
     #3164 students with a disability
     # looks at the number of students who pass/withdrawn/fail/distinction 
@@ -48,7 +53,7 @@ def disabilityDataSet():
 
    
     # print(studentResults)
-    print(df8)
+    print(d4)
     
     
 
@@ -58,6 +63,9 @@ def regionDataSet():
     # Does the region the student lives in a factor on overall student performance 
     data = pd.read_csv("studentInfo.csv")
     data = data.set_index(['code_module', 'region'])
+    mapping = {'Withdrawn': 0, 'Fail': 1, 'Pass': 2, 'Distinction': 3}
+    data = data.replace({'final_result': mapping})
+    #mapping to change the final results into numeric value
     df1 = data.groupby('region')['final_result'].value_counts()
     #Counts the number of pass/withdrawn/fail/Distinction by the region
     df2 = data.groupby('code_module')['final_result'].value_counts()
@@ -74,7 +82,6 @@ def regionDataSet():
     #Counts final result value grouped by prev_attempts
     df8 = data.groupby('code_presentation')['final_result'].value_counts()
     #Counts final result value grouped by code_presentation
-
     # data = data.index
     studentResults = data[["id_student", "studied_credits", "final_result"]]
     studentResults = studentResults.sort_index()
@@ -82,7 +89,32 @@ def regionDataSet():
     
     # studentResults.to_csv('regionDataSet.csv')
     # writes dataframe into a csv file
-    print(df8)
+    print(data)
 
 
-regionDataSet()
+
+def studentAssesmentDataSet():
+    data = pd.read_csv("studentInfo.csv")
+    data1 = pd.read_csv("studentAssessment.csv")
+
+    mapping = {'Withdrawn': 0, 'Fail': 1, 'Pass': 2, 'Distinction': 3}
+    data = data.replace({'final_result': mapping})
+    data = data.set_index(['id_student'])
+    data1 = data1.set_index(['id_student'])
+
+    df_index = pd.merge(data, data1, right_index=True, left_index=True)
+
+    d1 = df_index[['code_module', 'code_presentation','id_assessment','score', 'final_result']]
+    #create a dataframe showing all records with with following columns. 
+    # It uses the merged data between studentInfo and studentAssessment 
+    d2 = df_index.groupby('code_module')['final_result'].value_counts()
+    #groups the code_module and counts the value of final_result
+    grouped = df_index.groupby('id_student').agg({'score': [min,max,mean]})
+    #shows the min/max/mean score of the student over all the assessments
+    region = df_index.groupby(['id_student', 'region','id_assessment']).agg({'score': ['max']})
+    #shows the value of the score the student got on each assessment 
+    grouped.to_csv('studentAssessmentScore.csv')
+    region.to_csv('regionStudentAssessment.csv')
+    # print(region) 
+
+studentAssesmentDataSet()
